727adbe418d378ea7660a25ff4e151091d37c4fd
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-16 22:02:14 +0500

    Update assignment2.py

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,32 +1,63 @@
-# install selenium for python using 'pip install selenium' in the terminal
+# install selenium wire for python using 'pip install selenium-wire' in the terminal
 # download and place chromedriver in the same directory as this file to successfully run this test
 
 import os
 import time
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 
-# finds the path for the current working directory and adds '/chromedriver' to the end of the path
-cwd = os.getcwd() + '/chromedriver'
 
-# creates a new instance of the chrome webdriver
-driver = webdriver.Chrome(executable_path=cwd)
+def createDriver():
+    # finds the path for the current working directory and adds '/chromedriver' to the end of the path
+    cwd = os.getcwd() + '/chromedriver'
 
-# goes to the url
-driver.get('http://www.google.com')
+    # creates a new instance of the chrome webdriver
+    driver = webdriver.Chrome(executable_path=cwd)
 
-# finds the search box and enters 'selenium' into it
-search = driver.find_element(By.CLASS_NAME,'gLFyf')
-search.send_keys('selenium')
-search.submit()
+    return driver
 
-# waits for the page to load by one second
-time.sleep(1)
+def basicFunctionality(driver):
+    # goes to the url
+    driver.get('http://www.google.com')
 
-# finds the first few results and outputs them to the console
-results = driver.find_elements(By.XPATH, ".//*[@id='rso']//div//h3/a")
-for result in results:
-    print(result.get_attribute("href"))
+    # finds the search box and enters 'selenium' into it
+    search = driver.find_element(By.CLASS_NAME,'gLFyf')
+    search.send_keys('selenium')
+    search.submit()
 
-time.sleep(1)
-driver.quit()
+    # waits for the page to load by one second
+    time.sleep(1)
+
+    # finds the first few results and outputs them to the console
+
+    print("First few search result URLs:")
+    results = driver.find_elements(By.XPATH, ".//*[@id='rso']//div//h3/a")
+    for result in results:
+        print(result.get_attribute("href"))
+    print("______________________________________________________________")
+    
+    return driver
+
+def apiTesting(driver):
+    # all API calls made by the webdriver are recorded and can be retrieved using the following functions
+
+    print("API testing using Selenium")
+    print("Format: method, host, status code, date and time")
+
+    # gets the list of all API calls made by the webdriver and prints out the first 10 which include all get and post requests
+    for each in driver.requests[:10]:
+         if each.response:
+            print(each.method, each.host, each.response.status_code, each.response.date, end='\n')
+    print("______________________________________________________________")
+    return driver
+
+def main():
+    
+    driver = createDriver()
+    driver = basicFunctionality(driver)
+    driver = apiTesting(driver)
+    time.sleep(1)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
